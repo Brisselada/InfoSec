7032128a6b4578e347457ca0cedfa4a2b41b3115
Bauke Risselada <riss1500@student.nhl.nl>
2020-09-29 00:17:18 +0200

    KnapsackEncryption 10/10, so now all assignments are functionally complete

--- Assignment 1/src/main/java/KnapsackEncryption.java
+++ Assignment 1/src/main/java/KnapsackEncryption.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.math.*;
 import java.io.InputStreamReader;
 
 public class KnapsackEncryption {
@@ -10,18 +11,16 @@ public class KnapsackEncryption {
             String encryptionMethod = br.readLine();
             if (encryptionMethod.charAt(0) == 'e') {
                 // Encryption
-//                System.out.println("Encryption");
                 String[] publicKeyStrings = br.readLine().split(" ");
                 int[] publicKey = stringArrToIntArr(publicKeyStrings);
-//                System.out.println("Found publicKey");
 
                 String line;
                 while ((line = br.readLine()) != null) {
-//                    System.out.println("line is " + line);
                     encrypt(publicKey, Integer.parseInt(line));
                 }
             }
             else {
+                // Decryption
                 String[] mn = br.readLine().split(" ");
                 int m = Integer.parseInt(mn[0]);
                 int n = Integer.parseInt(mn[1]);
@@ -32,8 +31,6 @@ public class KnapsackEncryption {
                 while ((line = br.readLine()) != null) {
                     decrypt(m, n, knapSack, Integer.parseInt(line));
                 }
-
-
             }
 
             br.close();
@@ -61,7 +58,6 @@ public class KnapsackEncryption {
 
     private static int[] intToBits(int j, int keyLength) {
         int[] result = new int[keyLength];
-//        int maxBits = (int) Math.pow(2, (keyLength)) - 1;
         int maxBits;
         for (int i = 0; i < keyLength; i++) {
             maxBits = (int) Math.pow(2, (keyLength - i - 1));
@@ -69,7 +65,31 @@ public class KnapsackEncryption {
                 result[keyLength - i - 1] = 1;
                 j -= maxBits;
             }
-//            maxBits /= 2;
+        }
+        return result;
+    }
+
+    private static int bitsToInt(int[] j) {
+        int keyLength = j.length;
+
+        int sum = 0;
+        for (int i = 0; i < keyLength; i++) {
+            if (j[i] == 1) {
+                sum += (int) Math.pow(2, i);
+            }
+        }
+        return sum;
+    }
+
+    private static int[] solveKnapsack(int[] knapSack, int target ) {
+        int keyLength = knapSack.length;
+        int[] result = new int[keyLength];
+
+        for (int i = (keyLength - 1); i >= 0; i--) {
+            if (knapSack[i] <= target) {
+                result[i] = 1;
+                target -= knapSack[i];
+            }
         }
         return result;
     }
@@ -77,11 +97,6 @@ public class KnapsackEncryption {
     private static void encrypt(int[] publicKey, int p) {
 
         int keyLength = publicKey.length;
-
-        // Get the max possible bits for the keyLength
-//        int maxBits = (int) Math.pow(2, (keyLength - 1)) + 1;
-        String StringBits = Integer.toBinaryString(p);
-//        int[] bits = stringToIntArr(StringBits);
         int[] bits = intToBits(p, keyLength);
         int sum = 0;
 
@@ -94,7 +109,20 @@ public class KnapsackEncryption {
     }
 
     private static void decrypt(int m, int n, int[] knapsack, int c) {
-        
-    }
 
+        // Determine the modular inverse of m
+        BigInteger bigM = BigInteger.valueOf(m);
+        BigInteger bigN = BigInteger.valueOf(n);
+        int mInverse = (bigM.modInverse(bigN)).intValue();
+
+        // Determine the value of the encrypted character
+        int cypherValue = (c * mInverse) % n;
+
+        // Retrieve bits of original character
+        int[] cypherBits = solveKnapsack(knapsack, cypherValue);
+
+        // Convert found bits
+        int result = bitsToInt(cypherBits);
+        System.out.println(result);
+    }
 }
