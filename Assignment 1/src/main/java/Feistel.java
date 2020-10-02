6c1aae7daaa2a3f70f94327b2ad69dae61cef03b
Jildert Venema <jildert.venema@hotmail.com>
2020-10-02 11:55:42 +0200

    Refactor feistel

--- Assignment 1/src/main/java/Feistel.java
+++ Assignment 1/src/main/java/Feistel.java
@@ -5,97 +5,68 @@ import java.util.Arrays;
 
 public class Feistel {
 
-    public static void main(String[] args) throws IOException {
-
-        //TODO: OPTIE 1: Proberen deze werkende te krijgen, loopt nu vast
-
+    /***
+     * Start a feistel cipher instance to read bytes from the console and encrypt it with the given arguments
+     * @param args The arguments for encrypting
+     */
+    public static void main(String[] args) {
         try {
-
             int nRead;
             byte[] data = new byte[16384];
             ByteArrayOutputStream buffer = new ByteArrayOutputStream();
 
-            while ((nRead = readLineWithTimeout(data, data.length)) != -1) {
+            while ((nRead = readLineWithTimeout(data, data.length, 0.2)) != -1) {
                 buffer.write(data, 0, nRead);
             }
 
             byte[] requestBytes = buffer.toByteArray();
 
-//            requestBytes = Files.readAllBytes(Path.of("src\\main\\resources\\feistel2.in"));
-            byte[] result = handleMessage(requestBytes);
-            System.out.write(result);
+            handleMessage(requestBytes);
 
-//            inputStream.close();
-
-        } catch (Exception e) {
-            System.out.println(e);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-
-
-//TODO: OPTIE 2: Pakt separator teken niet goed op met String -> byte[]
-
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        try {
-//            // Read the line
-//            StringBuilder request = new StringBuilder();
-//            request.append(br.readLine());
-//            String input = "";
-//
-//            do {
-//                try {
-//                    input = readLineWithTimeout(br);
-//                    if (input != null) {
-//                        request.append(input);
-//                    }
-//                } catch (IOException e) {
-//                    input = null;
-//                }
-//            } while (input != null);
-//
-////            request.append("\n");
-//
-//            br.close();
-//            String builderResult = request.toString();
-//            byte[] requestBytes = builderResult.getBytes();
-//            byte[] result = handleMessage(requestBytes);
-//            System.out.write(result);
-//
-////            byte[] fileArray = Files.readAllBytes(Path.of("src\\main\\resources\\feistel2.in"));
-//
-//        } catch (Exception e) {
-//            System.out.println(e);
-//        }
-
-
-
-          //TODO: OPTIE 3: Dit is de oude oplossing die werkte
-        //TODO: bytes should be read from input
-//        byte[] fileArray = Files.readAllBytes(Path.of("src\\main\\resources\\feistel2.in"));
-//        byte[] in = handleMessage(fileArray);
-//        byte[] out = Files.readAllBytes(Path.of("src\\main\\resources\\feistel2.out"));
-////        System.out.println("filearray: " + fileArray);
-//        System.out.println("\n");
-//        System.out.write(in);
-//        System.out.println("\n");
-//        System.out.write(out);
-
-
     }
 
-    /**
-     * Reads the line with a timeout
-     * @return Returns the line that has been read
-     * @throws IOException
+    /***
+     * Handle the input from the console and process feistel cipher.
+     * @param request Input bytes from the console.
+     * @throws IOException When printing the result goes wrong.
      */
-    private static int readLineWithTimeout(byte[] buffer, int nRead) throws IOException {
-        int x = 1; // wait 1 second at most
+    private static void handleMessage(byte[] request) throws IOException {
 
+        // Check encryption or decryption
+        boolean encrypt = request[0] == (byte) 'e';
+        int n = findSeparatorIndex(request);
+
+        // Get key and text from request
+        byte[] keyBytes = new byte[n - 2];
+        byte[] textBytes = new byte[request.length - n - 1];
+        System.arraycopy(request, 2, keyBytes, 0, n - 2);
+        System.arraycopy(request, n+1, textBytes, 0, request.length - n - 1);
+
+        byte[] result = FeistelCipher(textBytes, keyBytes, encrypt);
+        System.out.write(result);
+    }
+
+    /***
+     * Reads the line with a given timeout.
+     * @param buffer The buffer to write the bytes to.
+     * @param nRead Total number of bytes to read.
+     * @param timeoutSeconds Timeout in seconds.
+     * @return Returns the total number of bytes that have been read and -1 if no bytes where read.
+     * @throws IOException When reading the input goes wrong.
+     */
+    private static int readLineWithTimeout(byte[] buffer, int nRead, double timeoutSeconds) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         long startTime = System.currentTimeMillis();
-        while ((System.currentTimeMillis() - startTime) < x * 1000 && !br.ready()) {
-            // wait
+        while ((System.currentTimeMillis() - startTime) < timeoutSeconds * 1000 && !br.ready()) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         if (br.ready()) {
             return System.in.read(buffer, 0, nRead);
@@ -104,48 +75,35 @@ public class Feistel {
         }
     }
 
-
-    private static int findSeparatorIndex(byte[] reqBytes) {
+    /***
+     * Find the separator character (byte 0xFF) in given input bytes
+     * @param bytes Input bytes to find the separator in.
+     * @return The index of the separator. Returns -1 if the separator is not found.
+     */
+    private static int findSeparatorIndex(byte[] bytes) {
         byte target = (byte) 0xFF;
-        for (int b = 2; b < reqBytes.length; b++){
-            if (reqBytes[b] == target) {
+        for (int b = 2; b < bytes.length; b++){
+            if (bytes[b] == target) {
                 return b;
             }
         }
         return 0;
     }
 
-    private static byte[] encrypt(byte[] L, byte[] R, byte[] keyBytes, int iterations) {
-        byte[] prevL = R;
-        byte[] prevR = L;
+    /***
+     * Processes the feistel rounds based on the given iterations
+     * @param L Left side of the bytes.
+     * @param R Right side of the bytes.
+     * @param keyBytes The key that will be XOR'd.
+     * @param iterations The amount of iterations.
+     * @param encrypt True if the text has to be encrypted. False if it has to be decrypted.
+     * @return the result part.
+     */
+    private static byte[] processRounds(byte[] L, byte[] R, byte[] keyBytes, int iterations, boolean encrypt) {
 
-        for (int i = 0; i < iterations; i++) {
-            L = prevR;
-            R = prevL;
-
-            // Left is XOR'd, Right passes
-            byte[] tempL = new byte[4];
-            for (int k = 0; k < 4; k++) {
-                tempL[k] = (byte) ( L[k] ^ keyBytes[(i * 4) + k]);
-            }
-            L = tempL;
-            prevL = L;
-            prevR = R;
-        }
-
-        // Concatenate L and R
-        byte[] partResult = new byte[8];
-        System.arraycopy(R, 0, partResult, 0, 4);
-        System.arraycopy(L, 0, partResult, 4, 4);
-
-        return partResult;
-    }
-
-    private static byte[] decrypt(byte[] L, byte[] R, byte[] keyBytes, int iterations) {
-
-        // Determine what's left and what's right depending on iterations
-        byte[] prevL = iterations % 2 == 0 ? R : L;
-        byte[] prevR = iterations % 2 == 0 ? L : R;
+        // Determine what's left and what's right depending on encrypt or decrypt and iterations
+        byte[] prevL = encrypt ? R : (iterations % 2 == 0 ? R : L);
+        byte[] prevR = encrypt ? L : (iterations % 2 == 0 ? L : R);
 
         for (int i = 0; i < iterations; i++) {
             L = prevR;
@@ -169,43 +127,34 @@ public class Feistel {
         return result;
     }
 
-
-    private static byte[] handleMessage(byte[] request) {
-
-        // Check encryption or decryption
-        boolean encrypt = request[0] == (byte) 'e';
-        int n = findSeparatorIndex(request);
-
-        // Get key and text from request
-        byte[] keyBytes = new byte[n - 2];
-        byte[] textBytes = new byte[request.length - n - 1];
-        System.arraycopy(request, 2, keyBytes, 0, n - 2);
-        System.arraycopy(request, n+1, textBytes, 0, request.length - n - 1);
+    /***
+     * Process a feistel cipher on the given input text with the given key.
+     * @param text Input text to decrypt or encrypt.
+     * @param key Input key used for encryption or decryption.
+     * @param encrypt True if the text has to be encrypted. False if it has to be decrypted.
+     * @return The result bytes.
+     */
+    public static byte[] FeistelCipher(byte[] text, byte[] key, boolean encrypt) {
 
         // Counts how many times 4 fits in key length
-        int iterations = keyBytes.length / 4;
+        int iterations = key.length / 4;
 
         // Amount of separate textParts that should be processed
-        int textPartCount = textBytes.length / 8;
+        int textPartCount = text.length / 8;
 
-        byte[] result = new byte[textBytes.length];
+        byte[] result = new byte[text.length];
 
         // Process each part of text
         for (int t = 0; t < textPartCount; t++) {
             // Split textPart into two pieces
             byte[] L = new byte[4];
             byte[] R = new byte[4];
-            System.arraycopy(textBytes, (8 * t), L, 0, 4);
-            System.arraycopy(textBytes, (8 * t) + 4, R, 0, 4);
+            System.arraycopy(text, (8 * t), L, 0, 4);
+            System.arraycopy(text, (8 * t) + 4, R, 0, 4);
 
             byte[] partResult;
 
-            if (encrypt) {
-                partResult = encrypt(L, R, keyBytes, iterations);
-            }
-            else {
-                partResult =  decrypt(L, R, keyBytes, iterations);
-            }
+            partResult = processRounds(L, R, key, iterations, encrypt);
 
             // Append result with partResult
             System.arraycopy(partResult, 0, result, (8 * t), 8);
