72431d047391cb477b7787c9b09541fa7eb41b44
Jildert Venema <jildert.venema@hotmail.com>
2020-10-06 11:03:46 +0200

    Working curve sole

--- Assignment 1/src/main/java/ECC.java
+++ Assignment 1/src/main/java/ECC.java
@@ -1,2 +1,119 @@
-package PACKAGE_NAME;public class ECC {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class ECC {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 20000);
+
+
+//            double res = modInverse(-9, 5);
+
+//              TEST
+//            double[] res = ECC.solveCurve(1,4,3,1, 5, 2);
+
+//             TEST
+//            double[] res = ECC.computePointMultiplication(1,2,15,5, -7);
+
+
+            String[] input = br.readLine().split(",");
+
+            double x = Double.parseDouble(input[0].replace("(", ""));
+            double y = Double.parseDouble(input[1].replace(")", ""));
+
+            input = br.readLine().split(" ");
+
+            double a = Double.parseDouble(input[0]);
+            double b = Double.parseDouble(input[1]);
+            double p = Double.parseDouble(input[2]);
+
+            input = br.readLine().split(" ");
+            double m = Double.parseDouble(input[0]);
+            double n = Double.parseDouble(input[1]);
+
+
+            String result = ECC.ECCKeyExchange(x, y, a, b, p, m, n);
+            System.out.println(result);
+
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static String fmt(double d)
+    {
+        if(d == (long) d)
+            return String.format("%d",(long)d);
+        else
+            return String.format("%s",d);
+    }
+
+    private static double modulo(double a, double b) {
+        if (a < 0) {
+            double factor = Math.ceil(a * -1 / b);
+            a += factor * b;
+        }
+        return  a % b;
+    }
+
+    private static double modInverse(double a, double m)
+    {
+        BigInteger bga = BigDecimal.valueOf(a).toBigInteger();
+        BigInteger bgm = BigDecimal.valueOf(m).toBigInteger();
+
+        return bga.modInverse(bgm).doubleValue();
+    }
+
+
+    private static double[] solveCurve(double x1, double y1, double x2, double y2, double p, double a) {
+
+        double m;
+
+        if (x1 != x2 || y1 != y2) {
+            m = (y2 - y1) * modInverse((x2 - x1), p);
+        } else {
+            m = ((3 * x1 + a) / Math.pow((2 * y1), -1));
+        }
+
+        m = modulo(m, p);
+
+        double x3 = modulo(Math.pow(m, 2) - x1 - x2, p);
+        double y3 = modulo((m * (x1 - x3) - y1),  p);
+        return new double[] {x3, y3};
+    }
+
+    private static double[] computePointMultiplication(double x, double y, double multiplication, double p, double a) {
+        double[] Q = new double[] {x,y};
+        double[] N = new double[] {x,y};
+//        Q = ECC.solveCurve(Q[0], Q[1], N[0], N[1], p, a);
+
+        for (int i = 0; i < multiplication; i ++) {
+            Q = ECC.solveCurve(Q[0], Q[1], N[0], N[1], p, a);
+            // double points
+//            N[0] = N[0] * 2;
+//            N[1] = N[1] * 2;
+        }
+
+        return Q;
+    }
+
+    public static String ECCKeyExchange(double x, double y, double a, double b, double p, double m, double n) {
+
+        double[] alice = ECC.computePointMultiplication(x, y, m, p, a);
+        double[] bob = ECC.computePointMultiplication(x, y, n, p, a);
+
+        // The 2 points below should be the same.
+        double[] sharedSecretAlice = ECC.computePointMultiplication(bob[0], bob[1], m, p, a);
+        double[] sharedSecretBob = ECC.computePointMultiplication(alice[0], alice[1], n, p, a);
+
+
+
+        return "(" + fmt(sharedSecretAlice[0]) + ", " + fmt(sharedSecretAlice[1]) + ")";
+    }
+
+
 }
--- Assignment 1/src/main/java/RSA.java
+++ Assignment 1/src/main/java/RSA.java
@@ -1,2 +1,119 @@
-package PACKAGE_NAME;public class RSA {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class RSA {
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 20000);
+
+            boolean encrypt = br.readLine().equals("e");
+            String arguments = br.readLine();
+
+
+            StringBuilder inputText = new StringBuilder();
+            String input = "";
+            do {
+                try {
+                    input = RSA.readLineWithTimeout(br);
+                    if (input != null) {
+                        inputText.append(input).append("\n");
+                    }
+                } catch (IOException e) {
+                    input = null;
+                }
+            } while (input != null);
+
+            RSA.handleMessage(encrypt, arguments, inputText.toString());
+
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+
+    private static void handleMessage(boolean encrypt, String arguments, String inputText){
+        String[] args = arguments.split(" ");
+        double p = Double.parseDouble(args[0]);
+        double q = Double.parseDouble(args[1]);
+        double e = Double.parseDouble(args[2]);
+
+        String result = RSA.RSACipher(encrypt, p, q, e, inputText);
+        System.out.println(result.trim());
+
+    }
+
+
+    public static String RSACipher(boolean encrypt, double p, double q, double e, String inputText) {
+        double N = p * q;
+        double modulo = (p - 1) * (q - 1);
+
+        // Private key:
+        double d = modInverse(e, modulo);
+
+        // Public key: (N, e)
+
+        StringBuilder results = new StringBuilder();
+
+
+        for (String line : inputText.split("\n")) {
+            if (encrypt) {
+                double M = Double.parseDouble(line);
+                double C = RSA.repeatedSquare(M, e, N);
+                results.append((int) C).append('\n');
+            } else {
+                double C = Double.parseDouble(line);
+                double M = RSA.repeatedSquare(C, d, N);
+                results.append((int) M).append('\n');
+            }
+        }
+
+        return results.toString();
+    }
+
+    private static double repeatedSquare(double a, double b, double modulo) {
+        char[] binary = ("0" + Long.toBinaryString((long) b)).toCharArray();
+
+        double currentValue = 1;
+        for(int i = 0; i < binary.length-1; i ++) {
+            // If next bit is 1. add one
+            boolean addOne = i < binary.length - 1 && binary[i+1] == '1';
+            currentValue = (Math.pow(currentValue, 2) * (addOne ? a : 1)) % modulo;
+        }
+
+        return currentValue;
+    }
+
+    private static double modInverse(double a, double m)
+    {
+        BigInteger bga = BigDecimal.valueOf(a).toBigInteger();
+        BigInteger bgm = BigDecimal.valueOf(m).toBigInteger();
+
+        return bga.modInverse(bgm).doubleValue();
+    }
+
+    /**
+     * Reads the line with a timeout
+     * @param in The supplied BufferedReader
+     * @return Returns the line that has been read
+     * @throws IOException
+     */
+    private static String readLineWithTimeout(BufferedReader in) throws IOException {
+        double x = 0.2; // wait 0.2 second at most
+
+        long startTime = System.currentTimeMillis();
+        while ((System.currentTimeMillis() - startTime) < x * 1000 && !in.ready()) {
+            // wait
+        }
+        if (in.ready()) {
+            return in.readLine();
+        } else {
+            return null;
+        }
+    }
+
 }
