e2792f65c0756c67323b2ad039c040002cdf2696
Jildert Venema <jildert.venema@hotmail.com>
2020-10-14 13:24:19 +0200

    Tiger

--- python/tiger/tigerHash.py
+++ python/tiger/tigerHash.py
@@ -1,37 +1,6 @@
 from sboxes import S0, S1, S2, S3
-import sys
-import itertools
+import struct
 import math
-import time
-from operator import ixor
-import array
-import re
-
-def bitArrayToBytes(bitArray):
-    return [sum([byte[b] << b for b in range(0,64)])
-            for byte in zip(*(iter(bitArray),) * 64)]
-
-def tobits(s):
-    result = []
-    for c in s:
-        bits = bin(ord(c))[2:]
-        bits = '00000000'[len(bits):] + bits
-        result.extend([int(b) for b in bits])
-    return result
-
-def frombits(bits):
-    chars = []
-    for b in range(len(bits) // 8):
-        byte = bits[b*8:(b+1)*8]
-        chars.append(chr(int(''.join([str(bit) for bit in byte]), 2)))
-    return ''.join(chars)
-
-def intToBitArray(input):
-    s = "{0:b}".format(input)
-    res = []
-    for c in s:
-        res.append(int(c))
-    return res   
 
 def chunkInput(bytesInput):
 
@@ -41,9 +10,8 @@ def chunkInput(bytesInput):
     while(len(bytesInput) % 64 != 56):
         bytesInput.append(0x00)
     
-    messageLength64 = bytes([messageLength]) * 8
+    messageLength64 = (messageLength * 8).to_bytes(8, byteorder='big')
     bytesInput += messageLength64
-
 
     chunks = []
     amountOfChunks = math.ceil(len(bytesInput) / 64)
@@ -52,8 +20,15 @@ def chunkInput(bytesInput):
 
     return chunks
 
-def Fround(a, b, c, Wi, m):
+def bit8Tobit64(X):
+    res = []
+    amountOfItems = len(X) / 8
+    for i in range(int(amountOfItems)):
+        value = int.from_bytes(X[i*8:(i+1)*8], byteorder='big')
+        res.append(int(value))
+    return res
 
+def Fround(a, b, c, Wi, m):
     (a,b,c) = fm(a,b,c, Wi[0], m)
     (b,c,a) = fm(b,c,a, Wi[1], m)
     (c,a,b) = fm(c,a,b, Wi[2], m)
@@ -65,14 +40,6 @@ def Fround(a, b, c, Wi, m):
     return (a, b, c)
 
 def fm(a, b, c, x, m):
-
-    # c = c ^ x
-    # a = a - S0[((c) >> (0*8))&0xFF] ^ S1[((c) >> ( 2*8)) & 0xFF] ^ S2[((c) >> (4*8))&0xFF] ^ S3[((c) >> ( 6*8)) & 0xFF]
-    # b = b + S3[((c) >> (1*8))&0xFF] ^ S2[((c) >> ( 3*8)) & 0xFF] ^ S1[((c) >> (5*8))&0xFF] ^ S0[((c) >> ( 7*8)) & 0xFF]
-    # b = b * m
-
-
-    # Dit stukje is een voorbeeld gevonden op internet.
     c ^= x
     c &= 0xffffffffffffffff
     a -= S0[((c) >> (0*8))&0xFF] ^ S1[((c) >> ( 2*8)) & 0xFF] ^ S2[((c) >> (4*8))&0xFF] ^ S3[((c) >> ( 6*8)) & 0xFF]
@@ -84,30 +51,6 @@ def fm(a, b, c, x, m):
     return (a,b,c)
 
 def keySchedule(x):
-
-    # allf = 0xFFFFFFFFFFFFFFFF
-    # x[0] = x[0] - (x[7] ^ 0xA5A5A5A5A5A5A5A5)
-    # x[1] = x[1] ^ x[0]
-    # x[2] = (x[2] + x[1])
-
-    # # X3 is X3 min X2 XOR met de binary complement van X1 << 19 
-    # x[3] = x[3] - (x[2] ^ ((~x[1]&allf) << 19))
-    # x[4] = x[4] ^ x[3]
-    # x[5] = x[5] + x[4]
-    # x[6] = x[6] - (x[5] ^ ((~x[4]&allf) >> 23))
-    # x[7] = x[7] ^ x[6]
-    
-    # x[0] = x[0] + x[7]
-    # x[1] = x[1] - (x[0] ^ ((~x[7]&allf) << 19))
-    # x[2] = x[2] ^ x[1]
-    # x[3] = x[3] + x[2]
-    # x[4] = x[4] - (x[3] ^ ((~x[2]&allf) >> 23))
-    # x[5] = x[5] ^ x[4]
-    # x[6] = x[6] + x[5]
-    # x[7] = x[7] - (x[6] ^ 0x0123456789ABCDEF)
-
-
-    # Dit stukje is een voorbeeld gevonden op internet.
     allf = 0xFFFFFFFFFFFFFFFF
     x[0] = (x[0] - (x[7] ^ 0xA5A5A5A5A5A5A5A5)&allf ) & allf
     x[1] ^= x[0]
@@ -127,16 +70,8 @@ def keySchedule(x):
     x[7] = (x[7] - (x[6] ^ 0x0123456789ABCDEF)&allf ) & allf
     return x
 
-def outerRound(Wi):
-    
-    Wi = Wi.decode("utf-8")
-    Wi = tobits(Wi)
-    Wi = bitArrayToBytes(Wi)
-
-    # initial values a, b and c
-    a = 0x0123456789ABCDEF
-    b = 0xFEDCBA9876543210
-    c = 0xF096A5B4C3B2E187
+def outerRound(Wi, a, b, c):
+    Wi = bit8Tobit64(Wi)
 
     (a, b, c) = Fround(a, b, c, Wi, 5)
 
@@ -148,27 +83,28 @@ def outerRound(Wi):
 
     return (a,b,c)
 
-
 def tigerHash(X):
+    # initial values a, b and c
+    a = 0x0123456789ABCDEF
+    b = 0xFEDCBA9876543210
+    c = 0xF096A5B4C3B2E187
+
     for chunk in X:
-        (a,b,c) = outerRound(chunk)
-    result = "%016X%016X%016X" % (a, b, c)
-    return result 
+        (a,b,c) = outerRound(chunk, a, b, c)
+    return (a, b, c) 
 
 def main():
 
-    inputbytes = bytearray(input(), "utf-8")
+    s = input()
 
-    X = chunkInput(inputbytes)
+    bytesArray = bytearray()
+    bytesArray.extend(map(ord, s))
 
-    result = tigerHash(X)
-    print(result)
+    X = chunkInput(bytesArray)
 
-    # printChunk(a)
-    # printChunk(b)
-    # printChunk(c)
-    # hex
-    # print(result)
- 
+    (a, b, c) = tigerHash(X)
+
+    # print in hex, should be binary.
+    print("%016X%016X%016X" % (a, b, c))
  
 main()
